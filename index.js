7f7ca52c8e4dace329a0a446972b1fe63c24141a
ABANGA SAMUEL <psalmpally@gmail.com>
2023-02-05 08:16:03 +0100

    uodated the function

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "
 let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
-function fightNow() {
-    
+
+fightButton.addEventListener("click", function () {
+
     let randomIndexOne = Math.floor( Math.random() * fighters.length )
     let randomIndexTwo = Math.floor( Math.random() * fighters.length )
-    
+
     stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
-}
+})
